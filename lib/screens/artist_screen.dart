5cb1f9ab2d697a296a8ef976f88bd1b21f5f48b0
Yaretzi Ortiz <ortizyarezti@gmail.com>
2025-03-14 11:59:30 -0600

    Update artist

--- lib/screens/artist_screen.dart
+++ lib/screens/artist_screen.dart
@@ -33,6 +33,79 @@ class _ArtistScreenState extends State<ArtistScreen> {
     _cloudFirestoreService.deleteArtist('artistas', docId);
   }
 
+  void updateArtist(
+      String docId,
+      TextEditingController nombreController,
+      TextEditingController generoController,
+      TextEditingController albumsController,
+      TextEditingController inicioController) {
+    _cloudFirestoreService.updateArtist('artistas', docId, {
+      'nombre': nombreController.text,
+      'genero': generoController.text,
+      'albums': int.parse(albumsController.text),
+      'inicio': int.parse(inicioController.text),
+    });
+    nombreController.clear();
+    generoController.clear();
+    albumsController.clear();
+    inicioController.clear();
+  }
+
+  void showDialogEdit(ArtistModel artist) {
+    TextEditingController nombreController = TextEditingController();
+    TextEditingController generoController = TextEditingController();
+    TextEditingController albumsController = TextEditingController();
+    TextEditingController inicioController = TextEditingController();
+    // Recuperar los datos del objeto artist a los controladores
+    nombreController.text = artist.name;
+    generoController.text = artist.genre;
+    albumsController.text = artist.albums.toString();
+    inicioController.text = artist.start.toString();
+    // Mostrar el dialogo
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Editar artista'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: nombreController,
+                  decoration: const InputDecoration(labelText: 'Nombre'),
+                ),
+                TextField(
+                  controller: generoController,
+                  decoration: const InputDecoration(labelText: 'Género'),
+                ),
+                TextField(
+                  controller: albumsController,
+                  decoration: const InputDecoration(labelText: 'Álbums'),
+                ),
+                TextField(
+                  controller: inicioController,
+                  decoration: const InputDecoration(labelText: 'Inicio'),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Cancelar'),
+              ),
+              TextButton(
+                onPressed: () {
+                  updateArtist(artist.id, nombreController, generoController,
+                      albumsController, inicioController);
+                  Navigator.pop(context);
+                },
+                child: const Text('Actualizar'),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,7 +149,7 @@ class _ArtistScreenState extends State<ArtistScreen> {
                     return ListTile(
                       title: Text(artist.name),
                       subtitle: Text('Genero: ${artist.genre}'),
-                      onTap: null,
+                      onTap: () => showDialogEdit(artist),
                       trailing: IconButton(
                         icon: const Icon(Icons.delete),
                         onPressed: () => deleteArtist(artist.id),
